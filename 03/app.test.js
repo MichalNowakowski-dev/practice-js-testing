58785b03d91a2c287ff1793f3af15bb09d0fb461
MichalNowakowski-dev <nowakowski.m1995@gmail.com>
2024-06-17 20:48:29 +0200

    task03 first test added, but failed

--- 03/app.test.js
+++ 03/app.test.js
@@ -1,1 +1,7 @@
-import randomNumber from './app';
+import randomNumber from "./app";
+
+describe("randomNumber function", () => {
+  it("return 1 if min is 1 and max is 1", () => {
+    expect(randomNumber(1, 1).toBe(1));
+  });
+});
