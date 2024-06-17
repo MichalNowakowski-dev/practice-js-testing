5ccb9b470d4c83143cf836ec858f753a927dafd9
MichalNowakowski-dev <nowakowski.m1995@gmail.com>
2024-06-17 21:24:12 +0200

    task03 done

--- 03/app.js
+++ 03/app.js
@@ -1,3 +1,19 @@
 export default function randomNumber(min, max) {
+  // Sprawdzenie poprawności danych wejściowych
+  if (min > max) {
+    throw new Error("Wartość minimalna nie może być większa od maksymalnej!");
+  }
 
+  // Generowanie losowej liczby
+  const random = Math.random();
+  const range = max - min;
+  const adjustedRandom = random * range;
+  const result = min + adjustedRandom;
+
+  // Zaokrąglanie do żądanego typu (opcjonalnie)
+  if (Number.isInteger(min) && Number.isInteger(max)) {
+    return Math.floor(result);
+  } else {
+    return result;
+  }
 }
--- 03/app.test.js
+++ 03/app.test.js
@@ -2,6 +2,22 @@ import randomNumber from "./app";
 
 describe("randomNumber function", () => {
   it("return 1 if min is 1 and max is 1", () => {
-    expect(randomNumber(1, 1).toBe(1));
+    expect(randomNumber(1, 1)).toBe(1);
+  });
+  it("is in range 4-9", () => {
+    expect.assertions(2);
+    expect(randomNumber(4, 9)).toBeGreaterThanOrEqual(4);
+    expect(randomNumber(4, 9)).toBeLessThanOrEqual(9);
+  });
+  it("throws an Error if min > max", () => {
+    // expect(() => randomNumber(9, 5)).toThrow();
+    expect.assertions(1);
+    try {
+      return randomNumber(9, 5);
+    } catch (error) {
+      expect(error.message).toBe(
+        "Wartość minimalna nie może być większa od maksymalnej!"
+      );
+    }
   });
 });
